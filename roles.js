86db4fc9d37597b5cb5b39a00bb5df13ee9b9df1
DeepasriSelladurai <deepasriselladurai25@gmail.com>
2024-12-12 18:25:36 +0530

    Initial commit

---
+++ roles.js
@@ -0,0 +1,12 @@
+var typed = new Typed('.role', {
+    strings: [' Front End Developer',
+         ' Back End Developer',
+         ' Full Stack  Developer',
+         ' App Developer',
+         ' Software Engineer',
+         ' Python  Programmer'],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true 
+});
